8a224f1d9ce8288949b8676d9e07459b9a9b1386
di-osc <790990241@qq.com>
2025-09-15 19:37:34 +0800

    添加音频处理功能，新增Audio类以支持音频文件加载和显示，更新依赖项以包含docarray、requests和librosa，并添加示例音频文件。

--- osc_data/audio.py
+++ osc_data/audio.py
@@ -1,23 +1,68 @@
-import numpy as np
+from typing import Union
+from pathlib import Path
+from io import BytesIO
+import requests
 
-from ._core import audio
+from docarray import BaseDoc
+from docarray.typing import NdArray
+from pydantic import Field, ConfigDict
+import librosa
 
 
-def low_frame_rate(frames: np.ndarray, m: int, n: int) -> np.ndarray:
-    """apply low frame rate
+class Audio(BaseDoc):
+    """Audio object that represents an audio file.
+
     Args:
-        frames: input frames. shape: (batch_size, n_frames, n_features)
-        m: number of frames to combine
-        n: number of frames to skip
+        uri (Union[str, Path]): URL of the audio file or local path.
+        sample_rate (Optional[int], optional): Sample rate of the audio file. Defaults to None.
+        data (Optional[NdArray], optional): Data of the audio file. Defaults to None.
     """
 
-    return audio.low_frame_rate(frames, m, n)
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True, validate_assignment=True, extra="ignore"
+    )
+    uri: Union[str, Path] | None = Field(
+        None, description="URL of the audio file or local path."
+    )
+    sample_rate: int | None = Field(None, description="Sample rate of the audio file.")
+    data: NdArray = Field(None, description="Data of the audio file.")
 
+    def load(self, sample_rate: int | None = None, mono: bool | None = None) -> "Audio":
+        """Load the audio file from the URL."""
+        try:
+            if Path(self.uri).exists():
+                data, sample_rate = librosa.load(self.uri, sr=sample_rate, mono=mono)
+            else:
+                bytes_ = requests.get(self.uri).content
+                data, sample_rate = librosa.load(
+                    BytesIO(bytes_), sr=sample_rate, mono=mono
+                )
+            self.data = data
+            self.sample_rate = sample_rate
+        except Exception as e:
+            raise ValueError(f"Failed to load audio from {self.uri}. {e}")
+        return self
 
-def compute_decibel(frames: np.ndarray) -> np.ndarray:
-    """compute decibel
-    Args:
-        frames: input frames. shape: (n_frames, n_features)
-    """
+    def load_example(self) -> "Audio":
+        example_path = Path(__file__).parent / "assets" / "audio" / "example.wav"
+        self.uri = example_path
+        return self.load()
 
-    return audio.compute_decibel(frames)
+    def display(self):
+        """Display the audio file."""
+        from IPython.display import Audio as IPAudio
+
+        return IPAudio(
+            data=self.data,
+            rate=self.sample_rate,
+        )
+
+    @property
+    def duration_s(self):
+        """Get the duration of the audio file in seconds."""
+        return librosa.get_duration(y=self.data, sr=self.sample_rate)
+
+    @property
+    def duration_ms(self):
+        """Get the duration of the audio file in milliseconds."""
+        return self.duration_s * 1000
